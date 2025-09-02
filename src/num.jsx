91ece4e456002da72b2932a6c34a67f7dfba2660
sayma ruhi <saymaruhi@outlook.com>
2025-09-02 17:23:12 -0400

    added array

--- src/num.jsx
+++ src/num.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Num() {
 const numbers= Array.from({length:10}, (_,i)=>i);
-const evenNumbers= numbers.filter((num) => num % 2 !== 0)
+//const evenNumbers= numbers.filter((num) => num % 2 == 0)
 console.log(numbers)
    
   return (
     <div>
       <h2>Print no 0-10</h2>
      <ul style={{listStyle: 'none', color: "blue"}}>
-        {evenNumbers.map((num)=>(
+        {numbers.map((num)=>(
             <li key= {num}>number= {num}</li>
         ))}
      </ul>
